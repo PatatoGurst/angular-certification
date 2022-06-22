13c54a35ad93189845e0208c3603eb5dd5c7bee6
PatatoGurst <eguillot7@free.fr>
2022-06-22 11:23:49 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { StockAppComponent } from './stock-app.component';
 import { StockSearchConponent } from './stock/stock-search/stock-search.component';
 import { StockDetailsComponent } from './stock/stock-details/stock-details.component';
 import { StockService } from './stock/stock.service';
+import { stockAppRoutes } from './routes';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(stockAppRoutes)],
   declarations: [
     StockAppComponent,
     StockSearchConponent,
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,9 @@
+import { Routes } from '@angular/router';
 import { StockDetailsComponent } from './stock/stock-details/stock-details.component';
 import { StockSearchConponent } from './stock/stock-search/stock-search.component';
 
-export const stockAppRoutes = [
-  { path: 'stocks/search', component: StockSearchConponent },
+export const stockAppRoutes: Routes = [
+  { path: 'stocks', component: StockSearchConponent },
   { path: 'sentiment/:symbol', component: StockDetailsComponent },
+  { path: '', redirectTo: '/stocks', pathMatch: 'full' },
 ];
