8cec5e500aa18b7e9be59507bb9ff1069830835b
PatatoGurst <eguillot7@free.fr>
2022-06-24 20:07:56 +0200

    1 file modified

--- src/app/stock/stock-details/stock-details.component.ts
+++ src/app/stock/stock-details/stock-details.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { StockDetail } from '../../model/stock-detail';
 import { StockService } from '../stock.service';
+import {
+  faArrowUp,
+  faArrowDown,
+  faEquals,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   templateUrl: './stock-details.component.html',
 })
 export class StockDetailsComponent implements OnInit, OnDestroy {
   stock: StockDetail;
-
+  loading: boolean;
   subscriptions: Subscription[] = [];
+  
 
   constructor(
     private stockService: StockService,
@@ -20,12 +26,21 @@ export class StockDetailsComponent implements OnInit, OnDestroy {
   ngOnInit() {
     let today: Date = new Date();
     let dateTo = today.toISOString().split('T')[0];
-    today.setMonth(today.getMonth() - 3);
+    today.setMonth(today.getMonth() - 2);
     let dateFrom = today.toISOString().split('T')[0];
+    this.loading = true;
     this.subscriptions.push(
       this.stockService
         .getStockDetail(this.route.snapshot.params['symbol'], dateFrom, dateTo)
-        .subscribe((s) => (this.stock = s))
+        .subscribe(
+          (s) => {
+            this.loading = false;
+            return (this.stock = s);
+          },
+          (e) => {
+            this.loading = false;
+          }
+        )
     );
   }
 
